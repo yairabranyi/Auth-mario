863550e9d25eb254fd6bff14df3470641461edfd
Yair Abranyi <yairabranyi@gmail.com>
2020-10-28 23:40:22 +0200

    Seperation of the validation file

--- routs/auth.js
+++ routs/auth.js
@@ -1,48 +1,21 @@
 const router = require('express').Router()
 const User = require('../model/User')
-
-
-//Validation
-const Joi = require('joi')
-
-const schema = Joi.object({
-    username: Joi.string()
-        .alphanum()
-        .min(3)
-        .max(30)
-        .required(),
-    password: Joi.string()
-        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-    email: Joi.string()
-        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
-})
+const { registerValidation } = require('../validation')
 
 router.get('/', async (req, res) => {
+  //Data validation before saving the user
   res.send('Sending response from auth rout')
-
 })
-
-
-
 
 router.post('/register', async (req, res, next) => {
-     const { body } = req;
-    console.log("This is a mesage from Register POST");
-    console.log("body is: ", body);
-//Data validation befor we save a user:
- 
-try {
-    const value = await schema.validateAsync(body);
- res.send('Your user was created')
-
-}
-catch (err) {
-     res.send(err.details[0].message)
-
- } 
-
-
+  try {
+    const { error, value } = await registerValidation(req.body)
+    console.log('Your user was created: ')
+    res.send('Your user was created: ')
+  } catch (err) {
+    console.log(err.details[0].message)
+    return res.status(400).send(err.details[0].message)
+  }
 })
-
 
 module.exports = router
